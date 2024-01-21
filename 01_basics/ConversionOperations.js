71c0d7a78e94a3e277f8ffe9206ff71cd915711d
Atharv Mittal <138797801+stilln0thing@users.noreply.github.com>
2024-01-21 10:39:23 +0000

    conversion and operations

--- 01_basics/ConversionOperations.js
+++ 01_basics/ConversionOperations.js
@@ -1,10 +1,10 @@
 let score = null
 
-console.log(typeof score);
+// console.log(typeof score);
 
 let valueInNumber = Number(score);
-console.log(typeof valueInNumber);
-console.log(valueInNumber);
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
 
 
 //"33" => 33
@@ -14,12 +14,32 @@ console.log(valueInNumber);
 let isLoggedIn = "hfh"
 
 let booleanisLoggedIn = Boolean(isLoggedIn);
-console.log(booleanisLoggedIn);
+// console.log(booleanisLoggedIn);
 
 // 1 => true; // 0 => false
 // "" => false
 // "abcd" => true
 
-let someNumber = 33; 
-let stringNumber = String(someNumber);
-console.log(typeof stringNumber);
+// let someNumber = 33; 
+// let stringNumber = String(someNumber);
+// console.log(typeof stringNumber);
+
+// *******************Operations***********************
+
+let value =3;
+let negValue = -value;
+// console.log(negValue);
+
+// console.log(2**3); //power
+let str1 = "hello"
+let str2 = " Atharv"
+let str3 = str1+str2;
+// console.log(str3);
+
+// console.log(1 +"2");
+// console.log("1" +2);
+// console.log(1 +"2" + "2")
+// console.log(1 + 2+ "5");
+
+console.log(+true);
+console.log(+"");
