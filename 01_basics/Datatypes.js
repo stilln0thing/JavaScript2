1fdb4013690cc868a7f8bba8daac3873a294302e
Atharv Mittal <138797801+stilln0thing@users.noreply.github.com>
2024-01-21 08:45:16 +0000

    my 2nd commit

---
+++ 01_basics/Datatypes.js
@@ -0,0 +1,29 @@
+"use strict"; //treat all JS code as newer version
+
+// alert(3+1) we are using node.js not browser
+
+console.log(3+3);
+console.log("Atharv")
+
+let name = "Atharv"
+let age = 19
+let isLoggedIn = false
+let state;
+
+// number => 2^53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined => 
+// symbol => uniqueness
+
+
+// object 
+
+console.log(typeof undefined); //undefined
+console.log(typeof null); //object
+
+
+
+
