2bd912f7b13afbe74bc16052b7a797f912676507
Atharv Mittal <138797801+stilln0thing@users.noreply.github.com>
2024-01-25 06:01:28 +0000

    numbers and maths

---
+++ 01_basics/numandmaths.js
@@ -0,0 +1,19 @@
+const score = 400;
+// console.log(score);
+
+const balance = new Number(200);
+// console.log(balance);
+
+// console.log(balance.toString().length);
+// console.log(balance.toFixed(2));
+
+
+const otherNumber = 23.4576;
+//  console.log(otherNumber.toPrecision(4));
+
+ const hundreds = 100000;
+//  console.log(hundreds.toLocaleString('en-IN'));
+
+ //+++++++++++++++MATHS+++++++++++++++++++++++
+
+ console.log(Math);
---
+++ 01_basics/strings.js
@@ -0,0 +1,11 @@
+const name = "atharv";
+const repocount = 50;
+// console.log(name + repocount);
+
+console.log(`hello me name is ${name}`);
+
+
+
+const gameName = new String('atharv456');
+
+console.log(gameName[0]);
